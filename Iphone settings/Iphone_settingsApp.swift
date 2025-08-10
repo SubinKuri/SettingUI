ec025ffa56763f19e09f12aa191aaae265a7778a
adarsh <adarshanappan03@gmail.com>
2025-08-10 14:43:33 +0530

    Initial Commit

---
+++ Iphone settings/Iphone_settingsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Iphone_settingsApp.swift
+//  Iphone settings
+//
+//  Created by subin kurian on 10/08/25.
+//
+
+import SwiftUI
+
+@main
+struct Iphone_settingsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
